72fa5aa2a133abf99a3fc5fe983dd0d23f0196c2
ermirgurt <erik1297e@gmail.com>
2025-09-05 04:31:32 +0300

    feat: animate wings

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { Header, Hero, BackgroundElements, Wings } from './components';
 
 export const App = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
       <BackgroundElements />
       <main className="flex items-center justify-center flex-1 relative overflow-hidden">
         <Hero />
         <Wings />
       </main>
-    </>
+    </div>
   );
 };
--- src/components/Wings.tsx
+++ src/components/Wings.tsx
@@ -1,14 +1,80 @@
 import { motion } from 'framer-motion';
 
 export const Wings = () => {
+  const leftWingImages = [
+    {
+      src: '/images/wings/left/1.png',
+      width: 'w-[7.17719rem]',
+      right: '2rem',
+      top: '11rem',
+    },
+    {
+      src: '/images/wings/left/2.png',
+      width: 'w-[11.00031rem]',
+      right: '3rem',
+      top: '9rem',
+    },
+    {
+      src: '/images/wings/left/3.png',
+      width: 'w-[8.34675rem]',
+      right: '6rem',
+      top: '7rem',
+    },
+    {
+      src: '/images/wings/left/4.png',
+      width: 'w-[9.6165rem]',
+      right: '3rem',
+      top: '6rem',
+    },
+    {
+      src: '/images/wings/left/5.png',
+      width: 'w-[8.21394rem]',
+      right: '0',
+      top: '4rem',
+    },
+  ];
+
+  const rightWingImages = [
+    {
+      src: '/images/wings/right/1.png',
+      width: 'w-[8.66788rem]',
+      left: '0',
+      top: '2.5rem',
+    },
+    {
+      src: '/images/wings/right/2.png',
+      width: 'w-[9.3835rem]',
+      left: '3rem',
+      top: '0rem',
+    },
+    {
+      src: '/images/wings/right/3.png',
+      width: 'w-[9.91569rem]',
+      left: '3.5rem',
+      top: '0.5rem',
+    },
+    {
+      src: '/images/wings/right/4.png',
+      width: 'w-[8.28706rem]',
+      left: '3rem',
+      top: '2.5rem',
+    },
+    {
+      src: '/images/wings/right/5.png',
+      width: 'w-[8.67763rem]',
+      left: '0',
+      top: '1.5rem',
+    },
+  ];
+
   return (
-    <div className="absolute bottom-0 w-[58.92431rem] flex justify-center">
+    <div className="absolute bottom-10 w-[58.92431rem] flex justify-center">
       <div className="flex justify-center gap-20 flex-1">
         <motion.img
           src="/images/arc.svg"
           alt="arc"
-          className="h-[35.40763rem] w-auto absolute bottom-0 left-0"
-          initial={{ x: 800, opacity: 0, scale: 0.8 }}
+          className="h-[35.40763rem] w-auto absolute -bottom-[5rem] left-0"
+          initial={{ x: 300, opacity: 0, scale: 0.8 }}
           animate={{ x: 0, opacity: 1, scale: 1 }}
           transition={{
             type: 'spring' as const,
@@ -30,19 +96,32 @@ export const Wings = () => {
             duration: 1,
           }}
         />
-        <motion.img
-          src="/images/left-wing-content.png"
-          alt="left-wing-content"
-          className="absolute bottom-0 left-0 h-[35.40763rem] w-auto"
-          initial={{ opacity: 0, scale: 0.6, y: 80 }}
-          animate={{ opacity: 1, scale: 1, y: 0 }}
-          transition={{
-            type: 'spring' as const,
-            stiffness: 100,
-            damping: 12,
-            duration: 1,
-          }}
-        />
+        <div className="absolute bottom-0 left-0 h-[43rem] w-auto flex flex-col justify-center items-end">
+          {leftWingImages.map((image, index) => {
+            const lastIndex = leftWingImages.length - 1;
+            const distanceFromBottom = lastIndex - index;
+            const initialY = distanceFromBottom * 100;
+            const initialRotate = distanceFromBottom * -5;
+
+            return (
+              <motion.img
+                key={image.src}
+                src={image.src}
+                alt="left-wing-content"
+                className={`${image.width} relative`}
+                style={{ right: image.right, top: image.top }}
+                initial={{ y: initialY, rotate: initialRotate, x: 70 }}
+                animate={{ y: 0, rotate: 0, x: 0 }}
+                transition={{
+                  type: 'spring' as const,
+                  stiffness: 120,
+                  damping: 15,
+                  duration: 0.8,
+                }}
+              />
+            );
+          })}
+        </div>
       </div>
 
       <div className="relative flex justify-center flex-1">
@@ -61,8 +140,8 @@ export const Wings = () => {
         />
 
         <motion.div
-          className="h-[35.40763rem] w-auto absolute bottom-0 right-0"
-          initial={{ x: -800, opacity: 0, scale: 0.8 }}
+          className="h-[35.40763rem] w-auto absolute -bottom-12 right-0"
+          initial={{ x: -300, opacity: 0, scale: 0.8 }}
           animate={{ x: 0, opacity: 1, scale: 1 }}
           transition={{
             type: 'spring' as const,
@@ -78,19 +157,32 @@ export const Wings = () => {
           />
         </motion.div>
 
-        <motion.img
-          className="absolute bottom-0 right-0 h-[33.82581rem] w-auto"
-          src="/images/right-wing-content.png"
-          alt="right-wing-content"
-          initial={{ opacity: 0, scale: 0.6, y: 80 }}
-          animate={{ opacity: 1, scale: 1, y: 0 }}
-          transition={{
-            type: 'spring' as const,
-            stiffness: 100,
-            damping: 12,
-            duration: 1,
-          }}
-        />
+        <div className="absolute bottom-0 right-0 h-[33.82581rem] w-auto flex flex-col justify-between">
+          {rightWingImages.map((image, index) => {
+            const lastIndex = rightWingImages.length - 1;
+            const distanceFromBottom = lastIndex - index;
+            const initialY = distanceFromBottom * 100;
+            const initialRotate = distanceFromBottom * 10;
+
+            return (
+              <motion.img
+                key={image.src}
+                src={image.src}
+                alt="right-wing-content"
+                className={`${image.width} relative`}
+                style={{ left: image.left, top: image.top }}
+                initial={{ y: initialY, rotate: initialRotate, x: -70 }}
+                animate={{ y: 0, rotate: 0, x: 0 }}
+                transition={{
+                  type: 'spring' as const,
+                  stiffness: 120,
+                  damping: 15,
+                  duration: 0.8,
+                }}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
